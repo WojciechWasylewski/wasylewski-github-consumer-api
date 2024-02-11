26404a7187d2924f9403170456424182db8e1b2a
Wojciech Wasylewski <w.wasylewski@gmail.com>
2024-02-11 22:12:08 +0100

    Initial commit

---
+++ src/test/java/com/example/wasylewskigithubconsumerapi/WasylewskiGithubConsumerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wasylewskigithubconsumerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasylewskiGithubConsumerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
